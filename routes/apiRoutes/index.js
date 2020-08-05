18ab86fdf36332626a9abbee02f5f29225bb55b7
Cherie Cookson <cheriecookson@gmail.com>
2020-08-05 16:33:52 -0500

    fixed some issues with apiRoutes and json

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,49 +1,51 @@
 const router = require('express').Router();
-const { notes } = require('../db/db.json');
-const { uuidv4 } = require('uuid');
+const notes = require('../../db/db.json');
+const { uuid } = require('uuidv4');
 const fs = require("fs");
+const noteIDs = notes.map(note => note.id);
+const path = require('path');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+console.log(uuid());
+  res.json(notes);
 });
 
-router.get('/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
+// router.get('/notes/:id', (req, res) => {
+//   const result = findById(req.params.id, notes);
+//   if (result) {
+//     res.json(result);
+//   } else {
+//     res.send(404);
+//   }
+// });
 
-// How do I use uuidv4(); ??????????????
 
 router.post('/notes', (req, res) => {
-   req.body.id = notes.length.toString();
+  let newID = 0;
+  while(noteIDs.includes(newID)) {
+    newID++;
+  }
+   req.body.id = newID;
+   noteIDs.push(newID);
 
-    const note = createNewNote(req.body, notes);
+    const note = req.body;
+    notes.push(note);
     res.json(note);
 
     fs.writeFileSync(
-        path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes }, null, 2)
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(notes, null, 2)
       );
-      return notes;
+     
   
 });
 
-
-
-router.delete('/notes/:id', (req, res, next)  =>  {
-    Room.findByIdAndRemove(req.params.id, req.body, (err, post) => {
-     if (err) return next(err);
-     res.json(post);
-    });
-   });
+// router.delete('/notes/:id', (req, res, next)  =>  {
+//     note.findByIdAndRemove(req.params.id, req.body, (err, post) => {
+//      if (err) return next(err);
+//      res.json(post);
+//     });
+//    });
 
 
 module.exports = router;
