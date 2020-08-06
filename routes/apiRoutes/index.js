ec287035ca9adc64fedc3b20ebd274ece2168449
Cherie Cookson <cheriecookson@gmail.com>
2020-08-05 19:20:37 -0500

    updating to heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,24 +1,14 @@
 const router = require('express').Router();
 const notes = require('../../db/db.json');
-const { uuid } = require('uuidv4');
+// const { uuid } = require('uuidv4');
 const fs = require("fs");
 const noteIDs = notes.map(note => note.id);
 const path = require('path');
 
 router.get('/notes', (req, res) => {
-console.log(uuid());
+// console.log(uuid());
   res.json(notes);
 });
-
-// router.get('/notes/:id', (req, res) => {
-//   const result = findById(req.params.id, notes);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
-
 
 router.post('/notes', (req, res) => {
   let newID = 0;
@@ -36,17 +26,7 @@ router.post('/notes', (req, res) => {
         path.join(__dirname, '../../db/db.json'),
         JSON.stringify(notes, null, 2)
       );
-     
-  
 });
-
-// router.delete('/notes/:id', (req, res, next)  =>  {
-//     note.findByIdAndRemove(req.params.id, req.body, (err, post) => {
-//      if (err) return next(err);
-//      res.json(post);
-//     });
-//    });
-
 
 module.exports = router;
 
